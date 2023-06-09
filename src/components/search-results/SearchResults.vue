ab7fb30d2b732314665193c9c67793445efa6c44
Jandronit <jandrohen@gmail.com>
2023-06-09 19:25:20 +0200

    (lesson 345 course vue) - Display places in the results component

--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -1,11 +1,15 @@
 <script lang="ts">
+import { usePlacesStore } from '@/composables';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'SearchResults',
   setup() {
 
+    const { isLoadingPlaces, places } = usePlacesStore();
     return {
+        isLoadingPlaces,
+        places,
     };
   },
 });
@@ -13,21 +17,18 @@ export default defineComponent({
 
 <template>
 
-<ul class="list-group mt-3">
-
-<li class="list-group-item list-group-item-action">
-    <h5>Site name</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minus sunt ab enim qui culpa placeat.</p>
-    <div align="right">
-        <button class="btn btn-outline-primary btn-sm">
-            Adresses
-        </button>
+    <div v-if="isLoadingPlaces" class="alert alert-primary text-center">
+    <h5>Loading</h5>
+    <span>Places wait...</span>
     </div>
-</li>
 
-<li class="list-group-item list-group-item-action">
-    <h5>Site name</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minus sunt ab enim qui culpa placeat.</p>
+
+<ul v-else-if="places.length > 0"
+     class="list-group mt-3">
+
+<li v-for="place in places" :key="place.id" class="list-group-item list-group-item-action">
+    <h5>{{ place.text }}</h5>
+    <p>{{ place.place_name }}</p>
     <div align="right">
         <button class="btn btn-outline-primary btn-sm">
             Adresses
