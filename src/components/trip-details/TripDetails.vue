71d6ccbddb1da468c59c4c62259cf1f75e79256a
Jandronit <jandrohen@gmail.com>
2023-06-10 14:34:31 +0200

    (lesson 353 course vue) -  Show distance and duration in the display

---
+++ src/components/trip-details/TripDetails.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { useMapStore } from '@/composables';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TripDetails',
+  setup() {
+
+    const { distance, duration } = useMapStore();
+    return {
+        distance,
+        duration,
+    };
+  },
+});
+</script>
+
+<template>
+  <div v-if="distance" class="trip-container column">
+
+    <div class="col"> Distance: <strong>{{ distance }} kms</strong> </div>
+    <div class="col"> Time: <strong>{{ duration }} hora</strong> </div>
+  
+  </div>
+</template>
+
+<style scoped>
+.trip-container {
+    background-color: #fff;
+    border-radius: 10px;
+    bottom: 3.125rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    font-size: 1.5625rem;
+    left: 20px;
+    padding: 10px;
+    position: fixed;
+    width: 300px;
+}
+strong {
+    color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,8 @@
 
 <SearchBar />
 
+<TripDetails />
+
 </template>
 
 <script lang="ts">
@@ -15,13 +17,14 @@ import { defineComponent } from 'vue';
 import MapView from '@/components/mapview/MapView.vue';
 import MyLocationBtn from '@/components/my-location-btn/MyLocationBtn.vue';
 import SearchBar from '@/components/searchbar/SearchBar.vue';
-
+import TripDetails from '@/components/trip-details/TripDetails.vue';
 export default defineComponent({
   name: 'HomeView',
   components: {
     MapView,
     MyLocationBtn,
-    SearchBar
+    SearchBar,
+    TripDetails
 },
   setup() {
     usePlacesStore();
